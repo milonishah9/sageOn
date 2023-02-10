9d078262b2e982776eb6d1059f0974fd4f4e45a1
syndelldev <priyankap.syndell@gmail.com>
2023-02-10 16:38:45 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  
+useEffect(() => {
+  // POST request using axios inside useEffect React hook
+
+  const headers = { 
+    "X-Site": "webdevtestpurposeonly@gmail.com",
+    "X-Company": "webdevtestpurposeonly@gmail.com",
+    "ocp-apim-subscription-key": "dd59fe538242495cb4ce862fa3a74cc4",
+    "Authorization": 'Bearer c16326d99b364286b100100ba5afac8e',
+    "authorisation-sls-applicationClient-token": "dd59fe538242495cb4ce862fa3a74cc4" // Murano only
+  };
+
+  axios.get('https://api.columbus.sage.com/us/sageone/accounts/v3/attachmentss', { headers })
+
+  .then(function (response) {
+    console.log('sdf', response)
+  });
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
